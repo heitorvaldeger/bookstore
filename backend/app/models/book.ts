d8bd984312a6a9bd8e6313ab6f3bf7d7182c174e
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-16 21:42:48 -0300

    feat: create models

---
+++ backend/app/models/book.ts
@@ -0,0 +1,36 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import Order from './order.js'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+
+export default class Book extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column()
+  declare title: string
+
+  @column()
+  declare author: string
+
+  @column()
+  declare description: string
+
+  @column()
+  declare price: number
+
+  @column()
+  declare image: string
+
+  @column()
+  declare stock: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @manyToMany(() => Order)
+  declare orders: ManyToMany<typeof Order>
+}
---
+++ backend/app/models/order.ts
@@ -0,0 +1,18 @@
+import { DateTime } from 'luxon'
+import { BaseModel, column, manyToMany } from '@adonisjs/lucid/orm'
+import Book from './book.js'
+import type { ManyToMany } from '@adonisjs/lucid/types/relations'
+
+export default class Order extends BaseModel {
+  @column({ isPrimary: true })
+  declare id: number
+
+  @column.dateTime({ autoCreate: true })
+  declare createdAt: DateTime
+
+  @column.dateTime({ autoCreate: true, autoUpdate: true })
+  declare updatedAt: DateTime
+
+  @manyToMany(() => Book)
+  declare books: ManyToMany<typeof Book>
+}

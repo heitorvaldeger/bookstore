2cbc9d11bb5343ae20ccb03ca6b21e4df3df1bd4
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-16 22:13:12 -0300

    test: ensure returns books in database by category

--- backend/tests/unit/services/BookService.spec.ts
+++ backend/tests/unit/services/BookService.spec.ts
@@ -13,4 +13,18 @@ test.group('Services book service', (t) => {
 
     expect(books.length).toBe(10)
   })
+
+  test('it should return books in database by category', async ({ expect }) => {
+    const sut = new BookService()
+    const booksScience = await sut.getBooksByCategory('science')
+    expect(booksScience.length).toBe(10)
+
+    const booksBible = await sut.getBooksByCategory('bible')
+    expect(booksBible.length).toBe(0)
+
+    BookFactory.merge({ category: 'others' }).createMany(5)
+
+    const booksOthers = await sut.getBooksByCategory('others')
+    expect(booksOthers.length).toBe(5)
+  })
 })

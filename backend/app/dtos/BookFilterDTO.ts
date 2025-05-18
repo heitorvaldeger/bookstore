a27ef8ffc39604ecbd9e8a448ccac824ee90da8c
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-18 15:52:08 -0300

    feat: ensure return a books if filters is provided

---
+++ backend/app/dtos/BookFilterDTO.ts
@@ -0,0 +1,5 @@
+export interface BookFilterDTO {
+  title?: string
+  description?: string
+  author?: string
+}
--- backend/app/services/BookService.ts
+++ backend/app/services/BookService.ts
@@ -1,8 +1,12 @@
 import BookNotFoundException from '#exceptions/BookNotFoundException'
 import Book from '#models/book'
+import { ModelQueryBuilderContract } from '@adonisjs/lucid/types/model'
+import { BookFilterDTO } from '../dtos/BookFilterDTO.js'
 import { BookSaveDTO } from '../dtos/BookSaveDTO.js'
 
 export class BookService {
+  private filters: BookFilterDTO = {}
+
   async getAll() {
     return await Book.query().orderBy('id').select()
   }
@@ -11,6 +15,18 @@ export class BookService {
     return await Book.findManyBy({
       category,
     })
+  }
+
+  async getBooksByFilter(filters: BookFilterDTO) {
+    const qBook = Book.query()
+    this.filters = filters
+    const books = await this.filterByAuthor(qBook)
+      .filterByDescription(qBook)
+      .filterByTitle(qBook)
+      .filter(qBook)
+      .select()
+
+    return books
   }
 
   async create(book: BookSaveDTO) {
@@ -33,5 +49,33 @@ export class BookService {
     }
 
     await bookToDelete.delete()
+  }
+
+  private filterByAuthor(query: ModelQueryBuilderContract<typeof Book, Book>) {
+    if (this.filters.author) {
+      query.whereILike('author', `%${this.filters.author}%`)
+    }
+
+    return this
+  }
+
+  private filterByDescription(query: ModelQueryBuilderContract<typeof Book, Book>) {
+    if (this.filters.description) {
+      query.whereILike('description', `%${this.filters.description}%`)
+    }
+
+    return this
+  }
+
+  private filterByTitle(query: ModelQueryBuilderContract<typeof Book, Book>) {
+    if (this.filters.title) {
+      query.whereILike('title', `%${this.filters.title}%`)
+    }
+
+    return this
+  }
+
+  private filter(query: ModelQueryBuilderContract<typeof Book, Book>) {
+    return query
   }
 }

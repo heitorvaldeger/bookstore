c5915527999566dedf1295f818c1789f9b8a1df7
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-18 23:08:13 -0300

    test: ensure return 401 if credentials are incorrect

--- backend/tests/functional/AuthController.spec.ts
+++ backend/tests/functional/AuthController.spec.ts
@@ -1,4 +1,5 @@
 import User from '#models/user'
+import { errors } from '@adonisjs/auth'
 import { test } from '@japa/runner'
 
 test.group('Auth Controller', (group) => {
@@ -6,16 +7,27 @@ test.group('Auth Controller', (group) => {
   group.setup(async () => {
     user = await User.create({
       email: 'any_mail@mail.com',
-      password: '1234',
+      password: 'password',
     })
   })
 
   test('/POST - return 204 if credentials are correct', async ({ client, expect }) => {
     const response = await client.post('/auth/login').json({
       email: 'any_mail@mail.com',
-      password: '1234',
+      password: 'password',
     })
 
     expect(response.status()).toBe(204)
+  })
+
+  test('/POST - return 401 if credentials are incorrect', async ({ client, expect }) => {
+    const response = await client.post('/auth/login').json({
+      email: 'another_mail@mail.com',
+      password: 'another_password',
+    })
+
+    const { code, message } = new errors.E_INVALID_CREDENTIALS('Invalid user credentials')
+    expect(response.status()).toBe(401)
+    expect(response.body()).toEqual({ code, message })
   })
 })

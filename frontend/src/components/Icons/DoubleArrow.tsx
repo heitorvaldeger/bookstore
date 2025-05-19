4295c18aac1980e895370c43ae3e4a05efb0e841
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-19 18:15:15 -0300

    feat: add arrow icons to a component

---
+++ frontend/src/components/Icons/DoubleArrow.tsx
@@ -0,0 +1,26 @@
+export const DoubleArrow = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="13"
+      height="13"
+      viewBox="0 0 13 13"
+      fill="none"
+    >
+      <path
+        d="M3.25 8.66675L6.5 11.9167L9.75 8.66675"
+        stroke="black"
+        stroke-width="1.625"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+      <path
+        d="M9.75 4.33325L6.5 1.08325L3.25 4.33325"
+        stroke="black"
+        stroke-width="1.625"
+        stroke-linecap="round"
+        stroke-linejoin="round"
+      />
+    </svg>
+  );
+};

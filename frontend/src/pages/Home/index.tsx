7a3f789c5611285511fa88e62e0ff2bec58e5fa4
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-19 19:07:27 -0300

    feat: add slide for Home page

--- frontend/src/pages/Home/index.tsx
+++ frontend/src/pages/Home/index.tsx
@@ -10,6 +10,7 @@ import {
 
 import avatarImage from "../../assets/images/avatar.png";
 import logoImage from "../../assets/images/logo.png";
+import productSlide from "../../assets/images/product-slide.png";
 import { DoubleArrow } from "../../components/Icons/DoubleArrow";
 import { ArrowDown } from "../../components/Icons/ArrowDown";
 
@@ -64,21 +65,32 @@ export const Home = () => {
           <ShoppingBag size={17} />
         </header>
 
-        <main className="flex flex-1 justify-between">
-          <div className="relative">
-            <div className="absolute my-3 ml-3">
-              <Search size={12} className="text-slate-700" />
+        <main className="space-y-6">
+          <div className="flex flex-1 justify-between">
+            <div className="relative">
+              <div className="absolute my-3 ml-3">
+                <Search size={12} className="text-slate-700" />
+              </div>
+              <input
+                type="text"
+                placeholder="Pesquisar e filtrar"
+                className="border-[1px] w-xs rounded-full px-4 py-1 bg-white border-slate-900 pl-7"
+              />
             </div>
-            <input
-              type="text"
-              placeholder="Pesquisar e filtrar"
-              className="border-[1px] w-xs rounded-full px-4 py-1 bg-white border-slate-900 pl-7"
-            />
+            <button className="flex gap-2 items-center border-[1px] rounded-full px-2 border-slate-200 cursor-pointer">
+              <AlignLeft size={18} />
+              <span className="text-xs">Ordernar</span>
+            </button>
           </div>
-          <button className="flex gap-2 items-center border-[1px] rounded-full px-2 border-slate-200 cursor-pointer">
-            <AlignLeft size={18} />
-            <span className="text-xs">Ordernar</span>
-          </button>
+
+          <div className="flex flex-col items-center gap-2">
+            <img src={productSlide} />
+            <div className="flex gap-5">
+              <span className="inline-block w-2.5 h-2.5 bg-teal-700 rounded-full"></span>
+              <span className="inline-block w-2.5 h-2.5 bg-gray-500 rounded-full"></span>
+              <span className="inline-block w-2.5 h-2.5 bg-gray-500 rounded-full"></span>
+            </div>
+          </div>
         </main>
       </section>
     </main>

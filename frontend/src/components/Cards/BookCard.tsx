ac1164dd39b271bd4c1083221dd2813e608a6b80
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-20 17:52:48 -0300

    feat: add cursor-pointer class

--- frontend/src/components/Cards/BookCard.tsx
+++ frontend/src/components/Cards/BookCard.tsx
@@ -27,7 +27,10 @@ export const BookCard = ({
   };
 
   return (
-    <div onClick={handleClickBookCard} className="flex flex-col space-y-2">
+    <div
+      onClick={handleClickBookCard}
+      className="flex flex-col space-y-2 cursor-pointer"
+    >
       <div className="rounded-lg relative">
         {isNew && (
           <div className="mt-2 mx-2 w-fit font-inter absolute text-xs text-white bg-teal-700 rounded-tl-lg rounded-b-sm rounded-tr-sm px-2 py-1">

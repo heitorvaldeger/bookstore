7eb2f9f967d4f869f3883e360c636594e6f584ec
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-20 00:23:57 -0300

    feat: create default layout

---
+++ frontend/src/pages/_layouts/DefaultLayout.tsx
@@ -0,0 +1,17 @@
+import { Outlet } from "react-router";
+import { SideBarMenu } from "../../components/Menus/SideBarMenu";
+import { Header } from "../../components/Headers/Header";
+import { SubHeader } from "../../components/Headers/SubHeader";
+
+export const DefaultLayout = () => {
+  return (
+    <main className="h-screen flex">
+      <SideBarMenu />
+      <section className="bg-zinc-50 h-full w-full px-6 space-y-4">
+        <Header />
+        <SubHeader />
+        <Outlet />
+      </section>
+    </main>
+  );
+};

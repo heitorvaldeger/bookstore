c4b2cd303a49add79482a9eb56985580a2efc9d5
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-21 20:52:34 -0300

    feat: ensure return 422 if books list is empty on order

--- backend/app/validators/order.ts
+++ backend/app/validators/order.ts
@@ -3,11 +3,13 @@ import vine from '@vinejs/vine'
 export const createOrderValidator = vine.compile(
   vine.object({
     cliente: vine.string().maxLength(100),
-    books: vine.array(
-      vine.object({
-        id: vine.number().positive(),
-        quantidade: vine.number().positive(),
-      })
-    ),
+    books: vine
+      .array(
+        vine.object({
+          id: vine.number().positive(),
+          quantidade: vine.number().positive(),
+        })
+      )
+      .notEmpty(),
   })
 )

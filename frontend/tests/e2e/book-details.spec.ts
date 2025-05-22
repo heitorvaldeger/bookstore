606ffa89310be65083c7beac2369aa675e5ac488
heitorvaldeger <heitorvaldeger97@gmail.com>
2025-05-22 16:47:05 -0300

    test: create fixture to make reusable

--- frontend/tests/e2e/book-details.spec.ts
+++ frontend/tests/e2e/book-details.spec.ts
@@ -1,23 +1,8 @@
 import { Messages } from "@/constans/messages";
-import { BookCategoryEnum } from "@/models/book";
-import { test, expect } from "@playwright/test";
+import { test, expect } from "./_custom.fixture";
 
-test.beforeAll(async ({ request }) => {
-  await request.post(`${process.env.VITE_API_URL}/books`, {
-    data: {
-      titulo: "Livro de Teste",
-      autor: "Autor de Teste",
-      descricao: "Descricao de Teste",
-      categoria: BookCategoryEnum.SCIENCE,
-      estoque: 100,
-      imagem: "",
-      preco: 9999,
-    },
-  });
-});
-
-test("book details cart actions", async ({ page }) => {
-  await page.goto("/books/1", { waitUntil: "networkidle" });
+test("book details cart actions", async ({ page, book }) => {
+  await page.goto(`/books/${book?.id}`, { waitUntil: "networkidle" });
 
   await page.getByRole("button", { name: "Adicionar à sacola" }).click();
 
@@ -32,20 +17,9 @@ test("book details cart actions", async ({ page }) => {
   await expect(page.getByTestId("cart-quantity")).toContainText("1");
 });
 
-test("show book details with correct values", async ({ page }) => {
-  await page.goto("/books/1", { waitUntil: "networkidle" });
+test("show book details with correct values", async ({ page, book }) => {
+  await page.goto(`/books/${book?.id}`, { waitUntil: "networkidle" });
 
-  await expect(page.getByText("A Brief History of Time")).toBeVisible();
-  await expect(page.getByText("R$ 45,90")).toBeVisible();
-});
-
-test("navigate to book detail page", async ({ page }) => {
-  await page.goto("/", { waitUntil: "networkidle" });
-
-  await page
-    .getByRole("link", { name: "A Brief History of Time," })
-    .first()
-    .click();
-
-  expect(page.url()).toContain("/books/1");
+  await expect(page.getByTestId("book.title-author")).toBeVisible();
+  await expect(page.getByTestId("book.price")).toBeVisible();
 });
--- frontend/tests/e2e/book-search.spec.ts
+++ frontend/tests/e2e/book-search.spec.ts
@@ -1,29 +1,33 @@
-import { test, expect } from "@playwright/test";
+import { test, expect } from "./_custom.fixture";
 
-test("navigate to book when input search is filled", async ({ page }) => {
+test("navigate to book when input search is filled", async ({ page, book }) => {
   await page.goto("/", { waitUntil: "networkidle" });
 
   await page
     .getByRole("textbox", { name: "Pesquisar e filtrar" })
-    .fill("cosmos");
+    .fill(book.titulo);
   await page
     .getByRole("textbox", { name: "Pesquisar e filtrar" })
     .press("Enter");
 
-  expect(page.url()).toContain("/books?q=cosmos");
+  expect(page.url()).toContain(
+    `/books?${new URLSearchParams({
+      q: book.titulo,
+    }).toString()}`
+  );
 });
 
-test("show book card with correct information", async ({ page }) => {
+test("show book card with correct information", async ({ page, book }) => {
   await page.goto("/", { waitUntil: "networkidle" });
 
   await page
     .getByRole("textbox", { name: "Pesquisar e filtrar" })
-    .fill("cosmos");
+    .fill(book.titulo);
   await page
     .getByRole("textbox", { name: "Pesquisar e filtrar" })
     .press("Enter");
 
   await page.waitForLoadState("networkidle");
 
-  expect(page.getByTestId("book-card-4")).toBeVisible();
+  expect(page.getByTestId(`book-card-${book.id}`)).toBeVisible();
 });

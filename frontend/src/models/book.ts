32c144b6392fe566e5c54d032d62e801ec016e4e
Heitor Aurélio <heitorvaldeger97@gmail.com>
2025-06-08 20:32:29 -0300

    fix: add optional operator to book fields

--- frontend/src/models/book.ts
+++ frontend/src/models/book.ts
@@ -10,9 +10,9 @@ export interface Book {
   id: number;
   titulo: string;
   autor: string;
-  descricao: string;
+  descricao?: string;
   preco: number;
-  imagem: string;
+  imagem?: string;
   estoque: number;
   categoria: BookCategoryEnum;
 }
